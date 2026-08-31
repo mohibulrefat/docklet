c3df0b00ac24914c0529fdf4e2095b62a6d9714f
Mirza Mohibul Hasan <mirzamohibul618@gmail.com>
2026-08-31 09:50:31 +0600

    init rust gtk4 project

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use gtk::glib;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
+const APP_ID: &str = "dev.docklet.Docklet";
+
+fn main() -> glib::ExitCode {
+    tracing_subscriber::fmt::init();
+
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_window);
+    app.run()
+}
+
+fn build_window(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Docklet")
+        .default_width(900)
+        .default_height(600)
+        .build();
+
+    window.present();
+}
